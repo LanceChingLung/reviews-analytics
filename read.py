308962e0e1ca87510c3e1b5575ab9dc16e40e0a5
Lance Kao <717126@gmail,com>
2019-05-25 15:34:25 +0800

    calculate length of reviews

--- read.py
+++ read.py
@@ -2,9 +2,16 @@
 
 data = []
 count = 0
+data_length = 0
+ave_data_lenght = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line.strip())
+	for review in f:
+		data.append(review.strip())
 		count +=1
+		data_length += len(review) 
 		if count % 1000 == 0:
 			print(len(data))
+ave_data_lenght = data_length / len(data)
+
+print('檔案讀取完成, 總共有', len(data), '筆資料')
+print('平均每筆資料長度:', ave_data_lenght)
